2da15abdb180c372497cbe2776d1e8be58c1610b
Simon Jäger <simon.jager@live.com>
2020-08-21 14:22:48 +0200

    Merge IDT and IRQ files (#32)

--- kernel/include/cpu/irq.h
+++ kernel/include/cpu/irq.h
@@ -2,7 +2,29 @@
 #define CPU_IRQ_H_
 
 #include <stdint.h>
-#include "cpu/idt.h"
+
+#define IDT_SIZE 0xFF
+
+#define IDT_GATE_TYPE_TASK 0x5
+#define IDT_GATE_TYPE_INTERRUPT_16 0x6
+#define IDT_GATE_TYPE_TRAP_16 0x7
+#define IDT_GATE_TYPE_INTERRUPT_32 0xE
+#define IDT_GATE_TYPE_TRAP_32 0xF
+
+typedef struct idt_entry
+{
+    uint16_t offset0_15;
+    uint16_t selector;
+    uint8_t reserved;
+    uint8_t flags;
+    uint16_t offset16_31;
+} __attribute__((packed)) idt_entry;
+
+typedef struct idt
+{
+    uint16_t size;
+    idt_entry *entries;
+} __attribute__((packed)) idt;
 
 typedef struct regs
 {
--- kernel/src/cpu/irq.c
+++ kernel/src/cpu/irq.c
@@ -70,66 +70,76 @@ extern void irq_handler(const regs *r)
     }
 }
 
+void irq_init_idt(idt_entry *entry, uint8_t selector, void (*handler)(), uint8_t access, uint8_t type)
+{
+    uint32_t address = (uint32_t)handler;
+    entry->offset0_15 = (address & 0xFFFF);
+    entry->selector = selector;
+    entry->reserved = 0;
+    entry->flags = 0x80 | type | ((access & 3) << 5);
+    entry->offset16_31 = (address >> 16) & 0xFFFF;
+}
+
 void irq_init()
 {
     /* By default every interrupt is initialized but ignored. */
     uint16_t segment = 0x08;
     for (size_t i = 0; i < IDT_SIZE; i++)
     {
-        init_idt_entry(&entries[i], segment, isr_ignore, 0, IDT_GATE_TYPE_INTERRUPT_32);
+        irq_init_idt(&entries[i], segment, isr_ignore, 0, IDT_GATE_TYPE_INTERRUPT_32);
     }
 
-    init_idt_entry(&entries[0], segment, isr0, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[1], segment, isr1, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[2], segment, isr2, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[3], segment, isr3, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[4], segment, isr4, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[5], segment, isr5, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[6], segment, isr6, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[7], segment, isr7, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[8], segment, isr8, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[9], segment, isr9, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[10], segment, isr10, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[11], segment, isr11, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[12], segment, isr12, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[13], segment, isr13, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[14], segment, isr14, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[15], segment, isr15, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[16], segment, isr16, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[17], segment, isr17, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[18], segment, isr18, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[19], segment, isr19, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[20], segment, isr20, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[21], segment, isr21, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[22], segment, isr22, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[23], segment, isr23, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[24], segment, isr24, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[25], segment, isr25, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[26], segment, isr26, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[27], segment, isr27, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[28], segment, isr28, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[29], segment, isr29, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[30], segment, isr30, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[31], segment, isr31, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[0], segment, isr0, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[1], segment, isr1, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[2], segment, isr2, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[3], segment, isr3, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[4], segment, isr4, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[5], segment, isr5, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[6], segment, isr6, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[7], segment, isr7, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[8], segment, isr8, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[9], segment, isr9, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[10], segment, isr10, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[11], segment, isr11, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[12], segment, isr12, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[13], segment, isr13, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[14], segment, isr14, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[15], segment, isr15, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[16], segment, isr16, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[17], segment, isr17, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[18], segment, isr18, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[19], segment, isr19, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[20], segment, isr20, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[21], segment, isr21, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[22], segment, isr22, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[23], segment, isr23, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[24], segment, isr24, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[25], segment, isr25, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[26], segment, isr26, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[27], segment, isr27, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[28], segment, isr28, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[29], segment, isr29, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[30], segment, isr30, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[31], segment, isr31, 0, IDT_GATE_TYPE_INTERRUPT_32);
 
-    init_idt_entry(&entries[32], segment, isr32, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[33], segment, isr33, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[34], segment, isr34, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[35], segment, isr35, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[36], segment, isr36, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[37], segment, isr37, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[38], segment, isr38, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[39], segment, isr39, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[40], segment, isr40, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[41], segment, isr41, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[42], segment, isr42, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[43], segment, isr43, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[44], segment, isr44, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[45], segment, isr45, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[46], segment, isr46, 0, IDT_GATE_TYPE_INTERRUPT_32);
-    init_idt_entry(&entries[47], segment, isr47, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[32], segment, isr32, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[33], segment, isr33, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[34], segment, isr34, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[35], segment, isr35, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[36], segment, isr36, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[37], segment, isr37, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[38], segment, isr38, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[39], segment, isr39, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[40], segment, isr40, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[41], segment, isr41, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[42], segment, isr42, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[43], segment, isr43, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[44], segment, isr44, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[45], segment, isr45, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[46], segment, isr46, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[47], segment, isr47, 0, IDT_GATE_TYPE_INTERRUPT_32);
 
-    init_idt_entry(&entries[128], segment, isr128, 0, IDT_GATE_TYPE_INTERRUPT_32);
+    irq_init_idt(&entries[128], segment, isr128, 0, IDT_GATE_TYPE_INTERRUPT_32);
 
     /* Load interrupt descriptor table register. */
     idt idtr;
